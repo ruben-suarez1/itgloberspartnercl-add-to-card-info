f97420814e9ef4d9dbf14aa87cbb048b883dc95f
ruben-suarez1 <rubensuarez199755@gmail.com>
2022-09-15 20:14:12 -0500

    Feat: add to a native component

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,12 +1,44 @@
 import React from "react"
 import { useProduct } from "vtex.product-context"
 import { useOrderForm } from "vtex.order-manager/OrderForm"
+import ButtonGroup from "./ButtonGroup"
 
 const AddToCartInfo = () => {
   const productInfo = useProduct()
-  const { orderForm } = useOrderForm()
+  const { orderForm: {
+    items,
+    totalizers
+  } } = useOrderForm()
   console.log("Este producto tiene esta info:", productInfo)
-  console.log("Esta es informacion de la orden:", orderForm)
+  console.log("Esta es informacion de la orden:", totalizers[0])
+
+  return (
+    <>
+      {
+        items.map((item: any, index: number) => {
+          return (
+            <div key={index}>
+              <div>
+                <img src={item.imageUrls.at1x} alt="imagen" />
+              </div>
+              <div>
+                <p>{item.name}</p>
+                <p>{item.id}</p>
+                <p>${item.price / 100}</p>
+                <p>Cant: {item.quantity}</p>
+                <p>SubTotal: $ {(item.price / 100) * (item.quantity)}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+      <div>
+        <p>Tenemos {items.length} items en tu compra</p>
+        <p>Total: ${totalizers[0]?.value / 100}</p>
+      </div>
+      <ButtonGroup />
+    </>
+  )
 }
 
 
---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const ButtonGroup = () => {
+  return (
+    <>
+      <div>
+        <a href="/">CHECKOUT</a>
+      </div>
+      <div>
+        <button>CONTINÚA COMPRANDO</button>
+        <a href="/">VER CARRITO</a>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
